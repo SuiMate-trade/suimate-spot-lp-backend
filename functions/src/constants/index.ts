e202f0dd5bcdb7143b8af519ee2ea5b9814639b9
Harsh <hkjal1605@gmail.com>
2024-05-28 01:59:48 +0530

    Spot trades alerts working

--- functions/src/constants/index.ts
+++ functions/src/constants/index.ts
@@ -1,1 +1,2 @@
 export const SUI_RPC_ENDPOINT = 'https://sui-mainnet-rpc.nodereal.io';
+export const SUI_DECIMALS = 9;
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,6 +8,7 @@
  */
 
 import { onRequest } from 'firebase-functions/v2/https';
+import { onValueWritten } from 'firebase-functions/v2/database';
 import * as logger from 'firebase-functions/logger';
 import { getTopSpotTradesController } from './controllers/topTradersController.js';
 import {
@@ -17,6 +18,13 @@ import {
 import { getPastSpotTradesController } from './controllers/pastSpotTradesController.js';
 import userFunctionApp from './routes/userRoutes.js';
 import alertsFunctionApp from './routes/alertsRoute.js';
+import { db } from './utils/firebase.js';
+import { SpotTradesDataType } from './types/dataTypes/spotTradesDataType.js';
+import { getCoinMetadata } from './utils/sui.js';
+import bot from './utils/bot.js';
+import { CoinMetadata } from '@mysten/sui.js/dist/cjs/client/index.js';
+import { toDecimalString } from './helpers/parseBignum.js';
+import { DateTime } from 'luxon';
 
 export const getTopSpotTraders = onRequest({ cors: true }, async (req, res) => {
   try {
@@ -89,3 +97,77 @@ export const getPastSpotTrades = onRequest({ cors: true }, async (req, res) => {
 
 export const userFunctions = onRequest(userFunctionApp);
 export const alertsFunctions = onRequest(alertsFunctionApp);
+
+// Trigger function to send telegram alerts when a traders places a trade
+export const sendTradeAlerts = onValueWritten(
+  `/swaps/{traderAddress}/{txnId}`,
+  async (event) => {
+    try {
+      const { traderAddress } = event.params;
+
+      const swapData: SpotTradesDataType = event.data.after.val();
+
+      const traderAlertsRef = await db
+        .collection('alerts')
+        .doc(traderAddress)
+        .get();
+      const traderAlertsList: string[] = traderAlertsRef.data()?.alerts || [];
+
+      const {
+        tokenAType,
+        tokenBType,
+        atob,
+        amountIn,
+        amountOut,
+        platform,
+        timestampMs,
+        txnDigest,
+      } = swapData;
+      const tokenAMetadata = (await getCoinMetadata(
+        tokenAType,
+      )) as CoinMetadata;
+      const tokenBMetadata = (await getCoinMetadata(
+        tokenBType,
+      )) as CoinMetadata;
+
+      const tokenInMetadata = atob ? tokenAMetadata : tokenBMetadata;
+      const tokenOutMetadata = atob ? tokenBMetadata : tokenAMetadata;
+
+      return await Promise.all(
+        traderAlertsList.map(async (userAddress) => {
+          try {
+            const userRef = await db
+              .collection('suimate-users')
+              .doc(userAddress)
+              .get();
+            const chatId = userRef.data()?.chatId;
+
+            await bot.sendMessage(
+              chatId,
+              `🚨 New spot exchange alert 🚨\n\n ${traderAddress} just swapped ${
+                tokenInMetadata.symbol
+              } for ${
+                tokenOutMetadata.symbol
+              } \n\n Swap Details: \n Amount In: ${toDecimalString(
+                amountIn,
+                tokenInMetadata.decimals,
+              )} ${tokenInMetadata.symbol} \n Amount Out: $${toDecimalString(
+                amountOut,
+                tokenOutMetadata.decimals,
+              )} ${tokenOutMetadata.symbol} \n Timestamp: ${DateTime.fromMillis(
+                timestampMs,
+              ).toFormat(
+                'hh:mm a, MM/dd/yyyy',
+              )} \n Platform: ${platform} \n\n Click here to view the trade: https://suivision.xyz/txblock/${txnDigest} \n\n Happy trading! 🚀`,
+            );
+          } catch (err) {
+            console.error(err);
+          }
+        }),
+      );
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  },
+);
